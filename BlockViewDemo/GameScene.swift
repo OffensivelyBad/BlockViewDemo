774f2ac10677c7f30308906c642e7f160358b5c8
SRoller <sroller@justfab.com>
2017-11-07 18:29:27 -0500

    Adds labels to the nodes

--- BlockViewDemo/GameScene.swift
+++ BlockViewDemo/GameScene.swift
@@ -11,79 +11,115 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var cameraIsZoomed = false
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        super.didMove(to: view)
+        setupCamera()
+        createBlocks(2000)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        guard !self.cameraIsZoomed else { return }
+        guard let touch = touches.first else { return }
+        self.cameraIsZoomed = true
+        zoomCameraInOnPoint(touch.location(in: self))
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        guard self.cameraIsZoomed else { return }
+        guard let touch = touches.first else { return }
+        panCameraToPoint(touch.previousLocation(in: self), endPoint: touch.location(in: self))
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        guard self.cameraIsZoomed else { return }
+        self.cameraIsZoomed = false
+        zoomCameraOut()
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        guard self.cameraIsZoomed else { return }
+        self.cameraIsZoomed = false
+        zoomCameraOut()
     }
-    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
+}
+
+// MARK: - Camera
+extension GameScene {
+    
+    func setupCamera() {
+        
+        let camera = SKCameraNode()
+        self.camera = camera
+        guard let cam = self.camera else { return }
+        self.addChild(cam)
+        
+    }
+    
+    func zoomCameraInOnPoint(_ point: CGPoint) {
+        guard let cam = self.camera else { return }
+        let scale = SKAction.scale(by: 0.25, duration: 0.25)
+        let position = SKAction.move(to: point, duration: 0.25)
+        cam.run(scale)
+        cam.run(position)
+    }
+    
+    func zoomCameraOut() {
+        guard let cam = self.camera else { return }
+        let scale = SKAction.scale(by: 4, duration: 0.25)
+        let position = SKAction.move(to: CGPoint.zero, duration: 0.25)
+        cam.run(scale)
+        cam.run(position)
+    }
+    
+    func panCameraToPoint(_ startPoint: CGPoint, endPoint: CGPoint) {
+        guard let cam = self.camera else { return }
+        let changeX = startPoint.x - endPoint.x
+        let changeY = startPoint.y - endPoint.y
+        cam.position.x += changeX
+        cam.position.y += changeY
+    }
+    
+}
+
+// MARK: - UI Block Creation
+extension GameScene {
+    
+    func createBlocks(_ qty: Int) {
+        
+        let blockSize = CGSize(width: 10, height: 10)
+        let colors: [UIColor] = [.red, .yellow, .blue, .green, .gray, .white, .cyan, .purple, .magenta, .orange]
+        let halfScreenWidth = self.size.width / 2
+        let halfScreenHeight = self.size.height / 2
+        
+        var currentX = -halfScreenWidth + (blockSize.width / 2)
+        var currentY = halfScreenHeight - (blockSize.height / 2)
+        
+        for i in 0..<qty {
+            
+            if currentX + blockSize.width > halfScreenWidth {
+                currentY -= blockSize.height
+                currentX = -halfScreenWidth + (blockSize.width / 2)
+            }
+            let color = colors[i % colors.count]
+            let block = SKSpriteNode(color: color, size: blockSize)
+            block.position = CGPoint(x: currentX, y: currentY)
+            
+            let labelNode = SKLabelNode(text: "\(i)")
+            labelNode.fontSize = 4
+            labelNode.position = CGPoint.zero
+            block.addChild(labelNode)
+            
+            self.addChild(block)
+            
+            currentX += blockSize.width
+        }
+        
+    }
+    
 }
